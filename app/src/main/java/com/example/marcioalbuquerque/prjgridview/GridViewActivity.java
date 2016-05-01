46807618df9fc077b16600f4cf7d8e593e9f4c0f
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-05-01 17:34:37 -0300

    Mondificação final da GridViewActivity
    Crianção final da CridViewActivity Java

--- app/src/main/java/com/example/marcioalbuquerque/prjgridview/GridViewActivity.java
+++ app/src/main/java/com/example/marcioalbuquerque/prjgridview/GridViewActivity.java
@@ -2,12 +2,29 @@ package com.example.marcioalbuquerque.prjgridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GridViewActivity extends AppCompatActivity {
-
+    GridView gridView;
+    static final String[] letras = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_view);
+        gridView = (GridView)findViewById(R.id.gridView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,letras);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),((TextView) view).getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
